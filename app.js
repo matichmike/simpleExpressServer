ad1487f12a145df7455a17bc96ff0c8028d8bc00
Michael Matich <matich.mike@gmail.com>
2021-03-11 17:44:06 -0500

    cors added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send("Hey there")
 });
 
 app.listen(port, () => {
-  console.log('Listening at 3000');
+  console.log('Listening at 3001');
 })
